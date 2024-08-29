9b41bdf8e9c90a757da9e1e55226fb9d641a6df9
gxbxD <gabrielldangelo@gmail.com>
2024-08-28 22:31:58 -0300

    (add) consume a API OpenFoodFacts

--- src/main/java/online/nutritabapi/api_nutricional/controller/AlimentoController.java
+++ src/main/java/online/nutritabapi/api_nutricional/controller/AlimentoController.java
@@ -1,40 +1,19 @@
 package online.nutritabapi.api_nutricional.controller;
 
-import online.nutritabapi.api_nutricional.model.Alimento;
 import online.nutritabapi.api_nutricional.service.AlimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/alimentos")
 public class AlimentoController {
-    
+
     @Autowired
     private AlimentoService alimentoService;
 
-    @PostMapping
-    public ResponseEntity<Alimento> adicionarAlimento(@RequestBody Alimento alimento) {
-        Alimento novoAlimento = alimentoService.salvarAlimento(alimento);
-        return new ResponseEntity<>(novoAlimento, HttpStatus.CREATED);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Alimento>> listarAlimentos() {
-        List<Alimento> alimentos = alimentoService.listarAlimentos();
-        return new ResponseEntity<>(alimentos, HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Alimento> obterAlimentoPorId(@PathVariable Long id) {
-        Alimento alimento = alimentoService.buscarPorId(id);
-        if (alimento != null) {
-            return new ResponseEntity<>(alimento, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/alimentos")
+    public String buscarAlimentos(@RequestParam String product_name_pt, @RequestParam(defaultValue = "1") int page) {
+        return alimentoService.buscarAlimentoPorNome(product_name_pt, page);
     }
 }
--- src/main/java/online/nutritabapi/api_nutricional/service/AlimentoService.java
+++ src/main/java/online/nutritabapi/api_nutricional/service/AlimentoService.java
@@ -1,25 +1,32 @@
 package online.nutritabapi.api_nutricional.service;
 
-import online.nutritabapi.api_nutricional.model.Alimento;
-import online.nutritabapi.api_nutricional.repository.AlimentoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AlimentoService {
-    @Autowired
-    private AlimentoRepository alimentoRepository;
+    private final RestTemplate restTemplate;
+    private final String BASE_URL = "https://world.openfoodfacts.net/api/v2/search";
+    private final int MAX_REQUESTS_PER_MINUTE = 10;
+    private int requestCount = 0;
+    private final long START_TIME = System.currentTimeMillis();
 
-    public Alimento salvarAlimento(Alimento alimento) {
-        return alimentoRepository.save(alimento);
+    public AlimentoService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public List<Alimento> listarAlimentos() {
-        return alimentoRepository.findAll();
-    }
-    public Alimento buscarPorId(Long id) {
-        return alimentoRepository.findById(id).orElse(null);
+    public String buscarAlimentoPorNome(String product_name_pt, int page) {
+        // Limitar a 10 requisições por minuto
+        if (requestCount >= MAX_REQUESTS_PER_MINUTE && (System.currentTimeMillis() - START_TIME) < 60000) {
+            throw new RuntimeException("Limite de requisições atingido. Tente novamente em breve.");
+        }
+
+        String url = BASE_URL + "?search_terms=product_name_pt&search_simple=1&action=process&json=1&page_size=10&page=" + page;
+
+        // Realizar a requisição
+        String resultado = restTemplate.getForObject(url, String.class);
+
+        requestCount++;
+        return resultado;
     }
 }
